8ce53dd717b3cae755f92113c2901cdb516137d5
spartan2375 <spartan9077@gmail.com>
2022-02-10 23:36:47 -0700

    comments, never heard of them

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Something about coding
+
+a wise man once said: "only comment code you care about"
+
+usage (linux only):
+g++ source.cpp -o out
--- source.cpp
+++ source.cpp
@@ -5,6 +5,10 @@
 
 void PartA();
 void PartB();
+void GetBootTime();
+void GetModeTime();
+void GetMemory();
+void GetProcTime();
 
 int main()
 {
@@ -24,9 +28,68 @@ void PartA()
     std::cout << "Machine name: " << sysInfo.machine << std::endl;
 }
 
+void PartB()
+{
+    GetProcTime();
+    GetBootTime();
+    GetModeTime();
+    GetMemory();
+}
+
+void GetMemory()
+{
+    std::string g, a, r, b, e;
+    long memTotal, memFree, memAvail;
+    std::ifstream file;
+    file.open("/proc/meminfo");
+
+    file >> g >> memTotal >> a >> r >> memFree >> b >> e >> memAvail;
+
+    if (g != "MemTotal:" || e != "MemAvailable:") // switch this back to the right var
+    {
+        std::cout << g << "[ERROR]: Could not parse memory info!\n";
+        return;
+    }
+
+    std::cout << g << " " << memTotal << std::endl
+              << e << " " << memAvail << std::endl;
+
+    file.close();
+}
+
+void GetModeTime()
+{
+    std::string asdf;
+    int userModeTime, garbage, systemModeTime;
+    std::ifstream file;
+    file.open("/proc/stat");
+    file >> asdf >> userModeTime >> garbage >> systemModeTime;
+    if (asdf != "cpu")
+    {
+        std::cout << "[ERROR]: Could not parse user and system mode times!\n";
+        return;
+    }
+    std::cout << "Time spent in user mode: " << userModeTime
+              << "\nTime spent in system mode: " << systemModeTime << std::endl;
+
+    file.close();
+}
+
 void GetBootTime()
 {
-    double upTime;
+    double upTime, garbage;
+    std::ifstream file;
+    file.open("/proc/uptime");
+    file >> upTime >> garbage;
+    int days = int(upTime) / 86400;
+    int hours = int(upTime) / 3600 % 24;
+    int minutes = int(upTime) / 60 % 60;
+    int seconds = int(upTime) % 60;
+
+    std::cout << "Time since last reboot: " << days << ":"
+              << hours << ":" << minutes << ":" << seconds << std::endl;
+
+    file.close();
 }
 
 void GetProcTime()
